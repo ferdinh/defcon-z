72ba408618b8de343e0db1690665ac69dcea1aeb
NeilRams <pfw9117@autuni.ac.nz>
2019-04-09 11:02:15 +1200

    Add Blank Method Docs

--- DEFCON Z/Assets/Scripts/Menu/MainMenu.cs
+++ DEFCON Z/Assets/Scripts/Menu/MainMenu.cs
@@ -6,6 +6,20 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,17 +28,5 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-  
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
