fcbdf0f528efaf3382b71e5b171af8ad1fd69713
Jordan Carter <alexkitteh@gmail.com>
2019-05-01 17:18:22 +1200

    Added Zombie Unit

---
+++ DEFCON Z/Assets/Scripts/Units/Zombie.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefconZ
+{
+    public class Zombie : UnitBase
+    {
+        public override void Update() { }
+    }
+}
+
