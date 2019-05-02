feb8a5859a08e71162331cdf5aea385b1f5f85db
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 14:48:46 +1200

    Add Modifier Type

---
+++ DEFCON Z/Assets/Scripts/Simulation/ModifierType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Type of modifier.
+/// </summary>
+public enum ModifierType
+{
+    Event,
+    Difficulty
+}
