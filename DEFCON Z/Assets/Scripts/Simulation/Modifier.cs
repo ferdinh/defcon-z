acff6b1695fc24803fbb92fb9b54957e045753b9
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 14:59:25 +1200

    Namespace fix

--- DEFCON Z/Assets/Scripts/Simulation/Modifier.cs
+++ DEFCON Z/Assets/Scripts/Simulation/Modifier.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
-/// A class that represents game modifiers.
-/// </summary>
-public class Modifier
+﻿namespace DefconZ.Simulation
 {
-    public string Name { get; set; }
-    public float Value { get; set; }
-    public ModifierType Type { get; set; }
+    /// <summary>
+    /// A class that represents game modifiers.
+    /// </summary>
+    public class Modifier
+    {
+        public string Name { get; set; }
+        public float Value { get; set; }
+        public ModifierType Type { get; set; }
+    }
 }
+
