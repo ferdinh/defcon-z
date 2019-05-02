cd369781fd76a293bd6dfa2f96fb371abe1817bb
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 14:48:00 +1200

    Add Resource class

---
+++ DEFCON Z/Assets/Scripts/Simulation/Resource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// A class that contains the available resources.
+/// </summary>
+public class Resource
+{
+    public float BaseResourcePoint { get; }
+    public float MaxResourcePoint { get; set; }
+    public float MaxSciencePoint { get; set; }
+    public float SciencePoint { get; set; }
+    public float ResourcePoint { get; set; }
+
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Resource"/> class.
+    /// </summary>
+    public Resource()
+    {
+        BaseResourcePoint = 10000.0f;
+    }
+
+}
