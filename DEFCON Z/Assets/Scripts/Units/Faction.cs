87c25e82763ee7f5c615263d1545fae04b862d55
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 15:03:11 +1200

    Add Faction Class

---
+++ DEFCON Z/Assets/Scripts/Units/Faction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefconZ.Units
+{
+    public class Faction : MonoBehaviour
+    {
+        public string FactionName { get; set; }
+        public IList<GameObject> Units { get; set; }
+        public FactionType FactionType { get; set; }
+        public bool IsHumanPlayer { get; set; } = false;
+
+        private void Awake()
+        {
+            Units = new List<GameObject>();
+        }
+
+}
