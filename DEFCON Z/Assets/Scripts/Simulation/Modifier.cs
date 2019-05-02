aed5e5114a6ffce4a8572f290990e64a36e306ad
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 14:49:05 +1200

    Add Modifier class
    The class is designed to hold Modifer values.

---
+++ DEFCON Z/Assets/Scripts/Simulation/Modifier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class that represents game modifiers.
+/// </summary>
+public class Modifier
+{
+    public string Name { get; set; }
+    public float Value { get; set; }
+    public ModifierType Type { get; set; }
+}
