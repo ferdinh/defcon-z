0452cfb077c98ce0c04fcf03d90bda49372a272d
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 15:24:52 +1200

    Add Resource to Faction class

--- DEFCON Z/Assets/Scripts/Units/Faction.cs
+++ DEFCON Z/Assets/Scripts/Units/Faction.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using DefconZ.Simulation;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DefconZ.Units
@@ -6,12 +7,19 @@ namespace DefconZ.Units
     public class Faction : MonoBehaviour
     {
         public string FactionName { get; set; }
+        public Resource Resource { get; set; }
         public IList<GameObject> Units { get; set; }
         public FactionType FactionType { get; set; }
         public bool IsHumanPlayer { get; set; } = false;
+        public GameObject UnitPrefab { get; internal set; }
 
         private void Awake()
         {
+            Resource = new Resource();
+
+            Resource.CalculateMaxPoints();
+            Resource.ComputeStartingValue();
+
             Units = new List<GameObject>();
         }
 
