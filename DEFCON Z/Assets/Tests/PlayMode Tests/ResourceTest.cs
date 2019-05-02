3cbd4ecf695a658a48c9052c951731cdf69fdc2b
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-02 16:48:07 +1200

    Add Test for Resource

---
+++ DEFCON Z/Assets/Tests/PlayMode Tests/ResourceTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefconZ.Simulation;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ResourceTest
+    {
+        
+        /// <summary>
+        /// Resources should not exceed maximum when being gathered.
+        /// </summary>
+        [Test]
+        public void Resource_ShouldNot_Exceed_Max()
+        {
+            // Arrange
+            Resource resource = new Resource();
+            resource.CalculateMaxPoints();
+            resource.ComputeStartingValue();
+
+            // Act
+            // Try adding the resources 1500 times/days. The base
+            // recovery rate to max manpower is at 1095 days/3years.
+            for (int i = 0; i < 1500; i++)
+            {
+                resource.GatherResource();
+            }
+
+            // Assert
+            Assert.AreEqual(resource.MaxResourcePoint, resource.ResourcePoint);
+
+        }
+
+    }
+}
