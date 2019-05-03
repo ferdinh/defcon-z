506e094753fd2b72857b5b13bd5eeafdea43d065
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-03 16:55:01 +1200

    Add base difficulty class

---
+++ DEFCON Z/Assets/Scripts/Simulation/Difficulty.cs
@@ -0,0 +1,32 @@
+﻿using DefconZ.Simulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Game difficulty preset.
+/// </summary>
+public static class Difficulty
+{
+    public static Modifier Easy = new Modifier
+    {
+        Name = "Easy Difficulty",
+        Type = ModifierType.Difficulty,
+        Value = 1.0f
+    };
+
+    public static Modifier Normal = new Modifier
+    {
+        Name = "Normal Difficulty",
+        Type = ModifierType.Difficulty,
+        Value = 0.0f
+    };
+
+    public static Modifier Hard = new Modifier
+    {
+        Name = "Hard Difficulty",
+        Type = ModifierType.Difficulty,
+        Value = -0.5f
+    };
+}
