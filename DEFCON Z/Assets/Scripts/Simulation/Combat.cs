e5b9dbbc15d1138eb0d6a7e3af0f83cf5938f8fb
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-04 16:18:35 +1200

    Create Combat.cs

---
+++ DEFCON Z/Assets/Scripts/Simulation/Combat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefconZ.Simulation
+{
+    public class Combat
+    {
+        public Guid CombatId { get; } = Guid.NewGuid();
+        public UnitBase FirstCombatant { get; set; }
+        public UnitBase SecondCombatant { get; set; }
+        public bool IsFighting { get; set; } = false;
+
+
+        public void Fight()
+        {
+            var damageToFirstCombatant = SecondCombatant.CalculateDamage();
+            var damageToSecondCombatant = FirstCombatant.CalculateDamage();
+
+            FirstCombatant.TakeDamage(damageToFirstCombatant);
+            SecondCombatant.TakeDamage(damageToSecondCombatant);
+        }
+    }
+}
