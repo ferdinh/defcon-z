ed74a04df2a57ba3af469d146e8634d52139f4a6
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-05 15:30:56 +1200

    Create ZombieFaction

---
+++ DEFCON Z/Assets/Scripts/Units/ZombieFaction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefconZ.Units
+{
+    public class ZombieFaction : Faction
+    {
+        protected override void InitStart()
+        {
+            base.InitStart();
+
+            UnitSpawnPoint = GameObject.Find("ZombieSpawnPoint");
+        }
+    }
+}
