9a24f15fc497f71c6b5aa808c3b3c8c18e0b3f96
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-05 15:31:06 +1200

    Create HumanFaction

---
+++ DEFCON Z/Assets/Scripts/Units/HumanFaction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefconZ.Units
+{
+    public class HumanFaction : Faction
+    {
+        protected override void InitStart()
+        {
+            base.InitStart();
+
+            UnitSpawnPoint = GameObject.Find("HumanSpawnPoint");
+        }
+    }
+}
