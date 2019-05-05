04a8c42e2dc088f8e6ec354b4765c7d6200a3783
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-05 15:05:16 +1200

    Add UnitPrefabList

---
+++ DEFCON Z/Assets/Scripts/Units/UnitPrefabList.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class UnitPrefabList : MonoBehaviour
+{
+    public static UnitPrefabList Instance = null;
+
+    public GameObject Human;
+    public GameObject Zombie;
+
+    /// <summary>
+    /// Awakes this instance.
+    /// </summary>
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
