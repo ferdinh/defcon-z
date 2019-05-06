f5321c33bafbd257a01ee5698c843a91ea9c3f7d
NeilRams <pfw9117@autuni.ac.nz>
2019-05-06 14:16:43 +1200

    Updated Difficulty Implementation

--- DEFCON Z/Assets/Scripts/Menu/SettingsMenu.cs
+++ DEFCON Z/Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+﻿using DefconZ.Simulation;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 namespace DefconZ
 {
@@ -14,18 +14,25 @@ namespace DefconZ
         /// Sets the difficulty by the provided level amount
         /// </summary>
         /// <param name="difficulty">Int from 1-3, higher the number, harder the difficulty</param>
+     
         public void SetDifficulty(int difficulty)
         {
             switch (difficulty)
             {
                 case 1:
                     Debug.Log("Difficulty set to Easy");
+                    Difficulty.SelectDifficulty = Difficulty.Easy;
+                    //gameManager.SetDifficulty(Difficulty.Easy);
+                    
                     break;
                 case 2:
                     Debug.Log("Difficulty set to Normal");
+                    Difficulty.SelectDifficulty = Difficulty.Normal;
                     break;
                 case 3:
                     Debug.Log("Difficulty set to Hard");
+                    //gameManager.SetDifficulty(Difficulty.Hard);
+                    Difficulty.SelectDifficulty = Difficulty.Hard;
                     break;
                 default:
                     break;
