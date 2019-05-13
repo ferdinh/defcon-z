a24a9cfc9d8b19c5b1e1bc4a12171316a1c11a93
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-13 22:11:23 +1200

    Update BaseTest.cs

--- DEFCON Z/Assets/Tests/PlayMode Tests/BaseTest.cs
+++ DEFCON Z/Assets/Tests/PlayMode Tests/BaseTest.cs
@@ -1,12 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace Tests
 {
+    /// <summary>
+    /// Base Class for tests. It provide the basic components that frequently
+    /// occurs when writing tests for the project.
+    /// </summary>
+    /// <remarks>
+    /// SetUp and TearDown attribute are safe to override. Include the call to
+    /// base class.
+    ///
+    /// GameObjects will be set to "active" by default.
+    /// </remarks>
     public class BaseTest
     {
+        protected GameObject _gameObject;
+
+        /// <summary>
+        /// Sets up test for every run.
+        /// </summary>
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _gameObject = new GameObject();
+        }
+
+        /// <summary>
+        /// Clean up test after each run.
+        /// </summary>
+        [TearDown]
+        public virtual void TearDown()
+        {
+            _gameObject = null;
+        }
     }
 }
