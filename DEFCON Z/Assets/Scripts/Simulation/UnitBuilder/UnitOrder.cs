ff78512556ab7afbeee8f9091a6852e08529cde1
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-21 10:05:32 +1200

    Add UnitOrder

---
+++ DEFCON Z/Assets/Scripts/Simulation/UnitBuilder/UnitOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefconZ.Simulation.UnitBuilder
+{
+    public class UnitOrder
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public GameObject unitPrefab;
+        public int recruitTime;
+        public Vector3 spawnPoint;
+
+        public UnitOrder(GameObject unitPrefab, Vector3 spawnPoint)
+        {
+            this.unitPrefab = unitPrefab;
+            recruitTime = unitPrefab.GetComponent<UnitBase>().RecruitTime;
+            this.spawnPoint = spawnPoint;
+        }
+    }
+}
