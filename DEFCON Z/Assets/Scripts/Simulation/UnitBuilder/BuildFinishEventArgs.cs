ef5af1466de8f6953578073d550280c35cfa72c5
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-21 10:05:11 +1200

    Add UnitBuilder EventArgs

---
+++ DEFCON Z/Assets/Scripts/Simulation/UnitBuilder/BuildFinishEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefconZ.Simulation.UnitBuilder
+{
+    public class BuildFinishedEventArgs : EventArgs
+    {
+        public GameObject createdUnit;
+
+        public BuildFinishedEventArgs(GameObject unitCreated)
+        {
+            createdUnit = unitCreated;
+        }
+    }
+}
---
+++ DEFCON Z/Assets/Scripts/Simulation/UnitBuilder/BuildProgressEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefconZ.Simulation.UnitBuilder
+{
+    public class BuildProgressEventArgs : EventArgs
+    {
+    }
+}
