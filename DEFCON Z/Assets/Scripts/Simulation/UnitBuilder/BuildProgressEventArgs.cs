47d750e1e176ffcf97df6a05778aec19b9a31ccd
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2019-05-21 13:22:55 +1200

    Update BuildProgressEventArgs.cs

--- DEFCON Z/Assets/Scripts/Simulation/UnitBuilder/BuildProgressEventArgs.cs
+++ DEFCON Z/Assets/Scripts/Simulation/UnitBuilder/BuildProgressEventArgs.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace DefconZ.Simulation.UnitBuilder
 {
+    /// <summary>
+    /// The <see cref="UnitOrder"/> progress information.
+    /// </summary>
+    /// <seealso cref="System.EventArgs" />
     public class BuildProgressEventArgs : EventArgs
     {
+        public int orderProgress;
+        public int recruitTime;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildProgressEventArgs"/> class.
+        /// </summary>
+        /// <param name="progress">The progress.</param>
+        /// <param name="recruitTime">The recruit time.</param>
+        public BuildProgressEventArgs(int progress, int recruitTime)
+        {
+            orderProgress = progress;
+            this.recruitTime = recruitTime;
+        }
     }
 }
